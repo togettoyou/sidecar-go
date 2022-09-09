d441ad1bb7380935ca1a1baf10f9853ae8f8858b
togettoyou <zoujh99@qq.com>
2022-09-09 10:58:09 +0800

    operator-sdk create api

---
+++ api/v1alpha1/sidecargo_types.go
@@ -0,0 +1,64 @@
+/*
+Copyright 2022.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package v1alpha1
+
+import (
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+)
+
+// EDIT THIS FILE!  THIS IS SCAFFOLDING FOR YOU TO OWN!
+// NOTE: json tags are required.  Any new fields you add must have json tags for the fields to be serialized.
+
+// SidecarGoSpec defines the desired state of SidecarGo
+type SidecarGoSpec struct {
+	// INSERT ADDITIONAL SPEC FIELDS - desired state of cluster
+	// Important: Run "make" to regenerate code after modifying this file
+
+	// Foo is an example field of SidecarGo. Edit sidecargo_types.go to remove/update
+	Foo string `json:"foo,omitempty"`
+}
+
+// SidecarGoStatus defines the observed state of SidecarGo
+type SidecarGoStatus struct {
+	// INSERT ADDITIONAL STATUS FIELD - define observed state of cluster
+	// Important: Run "make" to regenerate code after modifying this file
+}
+
+//+kubebuilder:object:root=true
+//+kubebuilder:subresource:status
+
+// SidecarGo is the Schema for the sidecargoes API
+type SidecarGo struct {
+	metav1.TypeMeta   `json:",inline"`
+	metav1.ObjectMeta `json:"metadata,omitempty"`
+
+	Spec   SidecarGoSpec   `json:"spec,omitempty"`
+	Status SidecarGoStatus `json:"status,omitempty"`
+}
+
+//+kubebuilder:object:root=true
+
+// SidecarGoList contains a list of SidecarGo
+type SidecarGoList struct {
+	metav1.TypeMeta `json:",inline"`
+	metav1.ListMeta `json:"metadata,omitempty"`
+	Items           []SidecarGo `json:"items"`
+}
+
+func init() {
+	SchemeBuilder.Register(&SidecarGo{}, &SidecarGoList{})
+}
